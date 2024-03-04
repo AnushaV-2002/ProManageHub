da47cc329bf04f90d2e6f3e22aaa3d8f98879a47
AnushaV-2002 <anushaviswanathan@outlook.com>
2024-03-04 17:32:08 +0530

    Error files corrected code

--- backend_server/index.js
+++ backend_server/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({extended: false}))
 app.use(cors());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://pro-manage-hub-inky.vercel.app");
+    res.header("Access-Control-Allow-Origin", "https://promanagehub.vercel.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
